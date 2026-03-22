3505e649ca1cd089c77ae7bbfa2d1dc40873987d
Jesus Joana <jesusjoanaazuara@gmail.com>
2026-03-22 11:14:00 +0100

    API RES minima generada doc mediante openapi

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
+
+const openapiDocument = YAML.load(
+    path.join(__dirname, '../openapi/openapi.yaml')
+);
 
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'API funcionando correctamente'
     });
 });
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiDocument));
 
 module.exports = app;
